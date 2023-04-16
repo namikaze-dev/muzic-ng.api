b0e9c26e324e6c807516fb48210602ca9558383c
namikaze-dev <usmansadik96@gmail.com>
2023-04-16 21:31:02 +0100

    Setup logger

---
+++ src/config/logger.js
@@ -0,0 +1,15 @@
+const { format, createLogger, transports } = require("winston");
+const config = require('./config');
+
+const devLogFormat = format.printf(({ level, message }) => {
+  return `[${level}]: ${message}`;
+});
+
+const logger = createLogger({
+  level: config.env === 'development' ? 'debug' : 'info',
+  format: config.env === 'development' ? devLogFormat : format.json(),
+  transports: [new transports.Console()],
+});
+
+module.exports = logger;
+  
