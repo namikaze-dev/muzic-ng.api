005b875911d816d772cadf205b65c0a2681f97a5
namikaze-dev <usmansadik96@gmail.com>
2023-04-17 07:07:58 +0100

    Setup api doc with swagger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDoc = require("./docs/swagger");
 const { songHandler } = require("./handlers");
 
 const app = express();
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use("/", songHandler);
 
---
+++ src/docs/swagger.js
@@ -0,0 +1,8 @@
+module.exports = {
+	openapi: "3.0.3",
+	info: {
+		title: "muzic.ng API",
+		description: "API server for muzic.ng",
+		version: "1.0.0"
+	}
+}
