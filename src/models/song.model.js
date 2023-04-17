24003b6af622c98755bcacdedae414becb20a635
namikaze-dev <usmansadik96@gmail.com>
2023-04-17 06:46:13 +0100

    Configure private fields for song model

--- src/models/song.model.js
+++ src/models/song.model.js
@@ -31,8 +31,23 @@ const Song = new mongoose.Schema(
         ],
     },
     {
-        timestamps: true,
+        timestamps: {
+            createdAt: "created_at",
+            updatedAt: "updated_at",
+        },
     }
 );
+
+// configure and hide private fields
+Song.methods.toJSON = function () {
+    var song = this.toObject();
+    song.id = song._id;
+    delete song._id;
+    delete song.category;
+    delete song.created_at;
+    delete song.updated_at;
+    delete song.__v;
+    return song;
+};
 
 module.exports = mongoose.model("Song", Song);
